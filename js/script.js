6ab759d0bc5d59d323e9f2882d6c312270ad3c7c
Vinícius Crizel <vinicius.crizel845@gmail.com>
2016-05-02 23:02:59 -0300

    subindo bootstrap mais conteudo -historico- adicionado como lorem ipsum

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 				scrollTop: (parseInt($("#"+x).offset().top) - 150) + "%"
 				  }, 1500);	
 		}
-		function aparece(){
-			var i = document.getElementsByTagName('head').offsetTop;
-			alert(i);
+		var cont = 0;
+		function teste(){
+			alert(cont);
+			cont++;
 		}
