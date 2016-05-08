3fb3129777450dd7d3fe1db401e130798b8aee8e
Vinícius Crizel <vinicius.crizel845@gmail.com>
2016-05-08 14:16:50 -0300

    option search

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 		function toMove(x){
 			$('html, body').animate({
-				scrollTop: (parseInt($("#"+x).offset().top) - 145) + "%"
+				scrollTop: (parseInt($("#"+x).offset().top) - 200) + "%"
 				  }, 1500);	
-		}
-		function somemenu(){
-			alert(document.getElementById('menu').offsetTop);
-			//if (document.getElementById('menu').offsetTop < 200){
-				//document.getElementById('menu').className = 'header-temp';
-				//document.getElementById('container').style = "display: none;";
-			//}
 		}
