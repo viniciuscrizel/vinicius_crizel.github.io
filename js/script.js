939022d86102e943e5a1beead91e109ac3757536
Vinícius Crizel <vinicius.crizel845@gmail.com>
2016-05-07 21:03:29 -0300

    algumas mudanças

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 		function toMove(x){
 			$('html, body').animate({
-				scrollTop: (parseInt($("#"+x).offset().top) - 150) + "%"
+				scrollTop: (parseInt($("#"+x).offset().top) - 145) + "%"
 				  }, 1500);	
+		}
+		function somemenu(){
+			alert(document.getElementById('menu').offsetTop);
+			//if (document.getElementById('menu').offsetTop < 200){
+				//document.getElementById('menu').className = 'header-temp';
+				//document.getElementById('container').style = "display: none;";
+			//}
 		}
